297ba744032b5ca81a23ac964ae2d0eb1e2f34a6
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2022-10-28 17:52:41 +0200

    Update Chet_login.py

--- frontend/Chet_login.py
+++ frontend/Chet_login.py
@@ -8,14 +8,16 @@ Created on Thu Oct 27 14:54:23 2022
 
 import streamlit as st
 import backend as be
+import webbrowser
 
 st.set_page_config( page_title="CHET", layout="centered")
 
-    
+# se credenziali giuste viene reindirizzato alla pagina di google per ora
+# se credenziali errate messaggio d'errore  
 def check_login():
     if be.login(supabase):
        placeholder.empty() 
-       exec(open("messageChat.py").read())
+       webbrowser.open("https://www.google.it")
     else:
         st.error('Username e/o password errati', icon="🚨")
       
@@ -24,17 +26,20 @@ placeholder = st.empty()
 
 with placeholder.container():
     st.title("CHET")
-    with st.form("login_form"):      
+    with st.form("my_form"):      
         st.session_state['Username'] = st.text_input("Username:")
         st.session_state['Password'] = st.text_input("Password:", type="password")
         b1 = st.form_submit_button("LOGIN")
         st.write('Non sei ancora registrato?')
-        b2 = st.form_submit_button("SIGN IN")
+        b2 = st.write(f'''
+                      <a target="_self" href="https://www.google.it?Username=st.session_state['Username']"> 
+                      <button>
+                      SIGN IN
+                      </button>
+                      </a>
+                      ''',
+                      unsafe_allow_html=True
+                      )
 
 if b1:
     check_login()
-
-if b2:
-    placeholder.empty()
-    exec(open("signIn.py").read())
-
