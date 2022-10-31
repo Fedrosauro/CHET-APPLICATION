d344cf4ad7466ec674e7e935bfeb3fd2e1269a12
Federico Pellizzaro <pellizzaro.federico16@gmail.com>
2022-10-31 11:15:55 +0100

    testing

--- Chet_login.py
+++ Chet_login.py
@@ -36,7 +36,7 @@ with placeholder.container():
         b1 = st.form_submit_button("LOGIN")
         st.write('Non sei ancora registrato?')
         b2 = st.write(f'''
-                      <a target="_self" href="https://www.google.it">
+                      <a target="_self" href="https://fedrosauro-prova-test-login-4m98ds.streamlitapp.com/">
                       <button>
                       SIGN IN
                       </button>
