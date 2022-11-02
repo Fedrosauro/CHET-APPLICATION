cdb6ccafda388ecfdf115a02cdeac975426c2332
Federico Pellizzaro <pellizzaro.federico16@gmail.com>
2022-11-02 15:32:57 +0100

    fix signin things

--- signIn.py
+++ signIn.py
@@ -8,11 +8,13 @@ import streamlit as st
 import backend as be
 import Chet_login as cl
 
+supabase = be.init_connection()
+
 #se passa il controllo si va alla chat
 #se non lo passa messaggio d'errore
 def check_signin():
-    if be.signin(supabase):
-        cl.nav_to("url_chat")
+    if be.signin(supabase, st.session_state["Username"], st.session_state['Password'], st.session_state['Mail']):
+        cl.nav_to("XXXXXX?Username=")
     else:
         st.error('Dati inseriti non corretti', icon="🚨")
 
@@ -20,17 +22,8 @@ st.title("Sign in")
 with st.form(key='form'):
     st.session_state['Username'] = st.text_input("Username")
     st.session_state['Password'] = st.text_input("Password", type="password")
-    st.session_state['Mail']=st.text_input("Mail")
+    st.session_state['Mail'] = st.text_input("Mail")
     signin_button=st.form_submit_button(label='Sign In')
 
 if signin_button:
     check_signin()
-    
-    
-    
-
-        
-
-
-
-
