c1073b5bfc47429e9c9ab02b42675f73b8f1ff6c
Federico Pellizzaro <pellizzaro.federico16@gmail.com>
2022-11-03 13:23:10 +0100

    fix

--- Chet_room.py
+++ Chet_room.py
@@ -39,7 +39,7 @@ if button:
 df= be.get_Database_dataFrame(supabase)
 for x in range(len(df.index)):
     series = df.loc[x]
-    messaget = series.at["User"] + "  \n"  series.at["Content"] + "  \n" + series.at["Time"]
+    messaget = series.at["User"] + "  \n" + series.at["Content"] + "  \n" + series.at["Time"]
     st.write(messaget)
     st.markdown("""series.at["User"]""")
     message(series.to_string(index=False, header=False))
