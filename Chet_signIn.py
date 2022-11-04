5abd6d2ab6c9a60e1cea73d57316b06714fba168
ldovu <115782431+ldovu@users.noreply.github.com>
2022-11-04 16:31:13 +0100

    Update Chet_signIn.py

--- Chet_signIn.py
+++ Chet_signIn.py
@@ -7,8 +7,28 @@ Created on Tue Oct 25 21:51:50 2022
 import streamlit as st
 import backend as be
 import Chet_login as cl
+import base64
 
 supabase = be.init_connection()
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+        .stApp {
+        background-image: url("data:image/png;base64,%s");
+        background-size: cover;
+        }
+    </style>
+    ''' % (bin_str)
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('schLogin3.png')
 
 #se passa il controllo si va alla chat
 #se non lo passa messaggio d'errore
