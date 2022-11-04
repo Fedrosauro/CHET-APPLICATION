c9e11f4c58f7639d83208d1a8b0d8ef65204ed80
simonecossaro <105944449+simonecossaro@users.noreply.github.com>
2022-11-04 11:37:05 +0100

    Update Chet_login.py

--- Chet_login.py
+++ Chet_login.py
@@ -34,7 +34,7 @@ def set_background(png_file):
 #funzione che reindirizza ad un url
 def nav_to(url):
     nav_script = """
-        <meta http-equiv="refresh" content="0; url='%s'">
+        <meta http-equiv="refresh" content="0; URL=javascript:window.open('http://google.com','_parent');">
     """ % (url)
     st.write(nav_script, unsafe_allow_html=True)
 
