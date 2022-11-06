bc0253e2a4778b5fe425658745ea53ce4c734e94
Federico Pellizzaro <pellizzaro.federico16@gmail.com>
2022-11-06 14:39:41 +0100

    testing different version

--- Chet_login.py
+++ Chet_login.py
@@ -46,6 +46,7 @@ def check_login():
         admin_value = be.get_admin_info(supabase, st.session_state["Username"])
         placeholder.empty()
         st.write("Benvenuto "+ st.session_state["Username"])
+        st.write(admin_value.loc[x])
         st.write(admin_value)
         chat_botton = st.write(f'''
                       <a href="https://fedrosauro-chet-application-chet-room-0dx65z.streamlit.app/?user=%s">
--- backend.py
+++ backend.py
@@ -53,5 +53,5 @@ def delete_message(supabase_conn, User_to_delete, Time_to_delete):
 
 ########################## GET ADMIN INFORMATION #############################
 def get_admin_info(supabase_conn, username):
-    return pd.Series(supabase_conn.table("Users").select("Admin").eq("Username", username).execute())
+    return pd.Dataframe(supabase_conn.table("Users").select("Admin").eq("Username", username).execute())
 ##############################################################################
