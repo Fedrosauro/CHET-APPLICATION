799bcb383f373955b001431d12a8b86b401c351e
Federico Pellizzaro <pellizzaro.federico16@gmail.com>
2022-11-06 14:20:09 +0100

    try 2

--- Chet_login.py
+++ Chet_login.py
@@ -46,7 +46,7 @@ def check_login():
         admin_value = be.get_admin_info(supabase, st.session_state["Username"])
         placeholder.empty()
         st.write("Benvenuto "+ st.session_state["Username"])
-        st.write("admin_value = " + admin_value)
+        st.write("admin_value = " + admin_value.at["Username"] + admin_value.at["Admin"])
         chat_botton = st.write(f'''
                       <a href="https://fedrosauro-chet-application-chet-room-0dx65z.streamlit.app/?user=%s">
                       <button>
