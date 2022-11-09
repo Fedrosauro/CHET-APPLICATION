9c453cdc16ee64f73102306d0e88641d60a0ff00
Pellizzaro Federico <67149530+Fedrosauro@users.noreply.github.com>
2022-11-09 22:19:12 +0100

    testing

--- Chet_room.py
+++ Chet_room.py
@@ -125,6 +125,8 @@ st.write(tabs_font_css, unsafe_allow_html=True)
 if st.experimental_get_query_params():
     value = st.experimental_get_query_params()["value"]
 st.session_state["Username"] = be.decode_string(value)
+st.write(st.session_state["Username"])
+
 
 st.markdown('<p class="big-font" style="font-family:Courier;color:Orange; font-size: 40px;">Chat </p>', unsafe_allow_html=True)
 
