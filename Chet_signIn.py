0abbb6f84cf8218470628e1dec1230020b071231
Pellizzaro Federico <67149530+Fedrosauro@users.noreply.github.com>
2022-11-10 17:55:50 +0100

    fixing code chet_signin

--- Chet_signIn.py
+++ Chet_signIn.py
@@ -2,50 +2,37 @@ import streamlit as st
 import backend as be
 import Chet_login as cl
 
+#initialize database connection
 supabase = be.init_connection()
 
 st.set_page_config(page_title="CHET", page_icon="💬")
 
 cl.set_background('back3.png')
 
-#se passa il controllo si va alla chat
-#se non lo passa messaggio d'errore
+#signin function returns TRUE or FALSE if the new user was correctly added or not
 def check_signin():
     if be.signin(supabase, st.session_state["Username"], st.session_state['Password'], st.session_state['Mail']):
+        #goes back to the login page
         cl.nav_to("https://fedrosauro-chet-application-chet-login-c65w2j.streamlit.app/")
     else:
         st.error('Dati inseriti non corretti', icon="🚨")
 
 tabs_font_css = """
-<style>
-div[class*="stTextInput"] label {
-  font-size: 20px;
-  font-family: "Courier New", monospace;
-  text-align: Center;
-  color: Orange;
-
-}
-
-div[class*="stTextInput"] label {
-  font-size: 20px;
-  font-family:  "Courier New", monospace;
-  text-align: Center;
-  color: Orange;
-}
-
-div[class*="stTextInput"] label {
-  font-size: 20px;
-  font-family:  "Courier New", monospace;
-  text-align: Center;
-  color: Orange;
-}
-</style>
-"""
+                    <style>
+                        div[class*="stTextInput"] label {
+                          font-size: 20px;
+                          font-family: "Courier New", monospace;
+                          text-align: Center;
+                          color: Orange;
+                        }
+                    </style>
+                """
 st.write(tabs_font_css, unsafe_allow_html=True)
 
 for i in range(5):
-        st.write("")
+    st.write("")
 
+#main code
 placeholder = st.empty()
 with placeholder.container():
     with st.form(key='form', clear_on_submit=True):
